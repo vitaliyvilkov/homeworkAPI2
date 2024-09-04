ffe14513c9ca42351d6b7803c7b0d26d589c4ce4
Vitaliy V <vitaliyvilkov@gmail.com>
2024-09-04 16:57:10 +0400

    initial commit

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,27 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldCheckoutPostmanEcho() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello world") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hello world"))
+        ;
+    }
+
+}
